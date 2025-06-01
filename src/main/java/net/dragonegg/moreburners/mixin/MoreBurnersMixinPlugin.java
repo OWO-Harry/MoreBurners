0c6d48ce73d10e866600a152f02adf6cfbbb4cab
OWO-Harry <harllyfly@163.com>
2025-06-01 11:18:06 +0800

    Fix issue #11

---
+++ src/main/java/net/dragonegg/moreburners/mixin/MoreBurnersMixinPlugin.java
@@ -0,0 +1,42 @@
+package net.dragonegg.moreburners.mixin;
+
+import net.dragonegg.moreburners.MoreBurners;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MoreBurnersMixinPlugin implements IMixinConfigPlugin {
+
+    public MoreBurnersMixinPlugin() {
+    }
+
+    public void onLoad(String mixinPackage) {
+    }
+
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return !(targetClassName.equals("com.simibubi.create.content.processing.basin.BasinRecipe") ||
+                targetClassName.equals("com.simibubi.create.content.fluids.tank.FluidTankBlockEntity") ||
+                targetClassName.equals("me.desht.pneumaticcraft.common.block.HeatPipeBlock")) || MoreBurners.loadedPNE();
+    }
+
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    public List<String> getMixins() {
+        return null;
+    }
+
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+}
